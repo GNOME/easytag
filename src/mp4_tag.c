0bdb71426af6fea70e646c2a54de6537d33a1d6f
Kip Warner <kip@thevertigo.com>
2012-06-18 00:06:26 -0700

    Honore Doktorr's revised mp4v2 patch...

--- src/mp4_tag.c
+++ src/mp4_tag.c
@@ -72,6 +72,8 @@
  *
  * Read tag data into an Mp4 file.
  *
+ * cf. http://mp4v2.googlecode.com/svn/doc/1.9.0/api/example_2itmf_2tags_8c-example.html
+ *
  * Note:
  *  - for string fields, //if field is found but contains no info (strlen(str)==0), we don't read it
  *  - for track numbers, if they are zero, then we don't read it
@@ -80,11 +82,12 @@ gboolean Mp4tag_Read_File_Tag (gchar *filename, File_Tag *FileTag)
 {
     FILE   *file;
     MP4FileHandle mp4file = NULL;
-		const MP4Tags *mp4tags;
+    const MP4Tags *mp4tags = NULL;
+    uint16_t track, track_total;
+    uint16_t disk, disktotal;
     Picture *prev_pic = NULL;
-#ifdef NEWMP4
     gint pic_num;
-#endif
+    const MP4TagArtwork *mp4artwork = NULL;
     
     if (!filename || !FileTag)
         return FALSE;
@@ -108,51 +111,53 @@ gboolean Mp4tag_Read_File_Tag (gchar *filename, File_Tag *FileTag)
         return FALSE;
     }
 
-		mp4tags = MP4TagsAlloc();
-		if (!MP4TagsFetch(mp4tags,mp4file)) {
-			gchar *filename_utf8 = filename_to_display(filename);
-			Log_Print(LOG_ERROR,_("Error reading tags from %s."),filename_utf8);
-			g_free(filename_utf8);
-			return FALSE;
-		}
+    mp4tags = MP4TagsAlloc();
+    if (!MP4TagsFetch(mp4tags, mp4file))
+    {
+        gchar *filename_utf8 = filename_to_display(filename);
+        Log_Print(LOG_ERROR,_("ERROR reading tags from file: '%s' (%s)."),filename_utf8,_("MP4 format invalid"));
+        g_free(filename_utf8);
+        return FALSE;
+    }
+
+    /* TODO Add error detection */
 
     /*********
      * Title *
      *********/
-		if (mp4tags->name)
-				FileTag->title = strdup(mp4tags->name);
-		else
-				FileTag->title = NULL;
+    if (mp4tags->name)
+        FileTag->title = g_strdup(mp4tags->name);
 
     /**********
      * Artist *
      **********/
-		if (mp4tags->artist)
-				FileTag->artist = strdup(mp4tags->artist);
-		else
-				FileTag->artist = NULL;
+    if (mp4tags->artist)
+        FileTag->artist = g_strdup(mp4tags->artist);
 
     /*********
      * Album *
      *********/
-		if (mp4tags->album)
-				FileTag->album = strdup(mp4tags->album);
-		else
-				FileTag->album = NULL;
+    if (mp4tags->album)
+        FileTag->album = g_strdup(mp4tags->album);
+
+    /****************
+     * Album Artist *
+     ****************/
+    if (mp4tags->albumArtist)
+        FileTag->album_artist = g_strdup(mp4tags->albumArtist);
 
     /**********************
      * Disk / Total Disks *
      **********************/
     if (mp4tags->disk)
     {
-        if (mp4tags->disk->index != 0 && mp4tags->disk->total != 0)
-            FileTag->disc_number = g_strdup_printf("%d/%d",
-																									 (gint)mp4tags->disk->index,
-																									 (gint)mp4tags->disk->total);
-        else if (mp4tags->disk->index != 0)
-            FileTag->disc_number = g_strdup_printf("%d",(gint)mp4tags->disk->index);
-        else if (mp4tags->disk->total != 0)
-            FileTag->disc_number = g_strdup_printf("/%d",(gint)mp4tags->disk->total);
+	disk = mp4tags->disk->index, disktotal = mp4tags->disk->total;
+        if (disk != 0 && disktotal != 0)
+            FileTag->disc_number = g_strdup_printf("%d/%d",(gint)disk,(gint)disktotal);
+        else if (disk != 0)
+            FileTag->disc_number = g_strdup_printf("%d",(gint)disk);
+        else if (disktotal != 0)
+            FileTag->disc_number = g_strdup_printf("/%d",(gint)disktotal);
         //if (disktotal != 0)
         //    FileTag->disk_number_total = g_strdup_printf("%d",(gint)disktotal);
     }
@@ -160,53 +165,45 @@ gboolean Mp4tag_Read_File_Tag (gchar *filename, File_Tag *FileTag)
     /********
      * Year *
      ********/
-		if (mp4tags->releaseDate)
-				FileTag->year = strdup(mp4tags->releaseDate);
-		else
-				FileTag->year = NULL;
+    if (mp4tags->releaseDate)
+        FileTag->year = g_strdup(mp4tags->releaseDate);
 
     /*************************
      * Track and Total Track *
      *************************/
     if (mp4tags->track)
     {
-        if (mp4tags->track->index != 0)
-            FileTag->track = g_strdup_printf("%.*d",NUMBER_TRACK_FORMATED_SPIN_BUTTON,(gint)mp4tags->track->index); // Just to have numbers like this : '01', '05', '12', ...
-        if (mp4tags->track->total != 0)
-            FileTag->track_total = g_strdup_printf("%.*d",NUMBER_TRACK_FORMATED_SPIN_BUTTON,(gint)mp4tags->track->total); // Just to have numbers like this : '01', '05', '12', ...
+
+	track = mp4tags->track->index, track_total = mp4tags->track->total;
+        if (track != 0)
+            FileTag->track = NUMBER_TRACK_FORMATED ? g_strdup_printf("%.*d",NUMBER_TRACK_FORMATED_SPIN_BUTTON,(gint)track) : g_strdup_printf("%d",(gint)track);
+        if (track_total != 0)
+            FileTag->track_total = NUMBER_TRACK_FORMATED ? g_strdup_printf("%.*d",NUMBER_TRACK_FORMATED_SPIN_BUTTON,(gint)track_total) : g_strdup_printf("%d",(gint)track_total);
     }
 
     /*********
      * Genre *
      *********/
-		if (mp4tags->genre)
-				FileTag->genre = strdup(mp4tags->genre);
-		else
-				FileTag->genre = NULL;
+    if (mp4tags->genre)
+        FileTag->genre = g_strdup(mp4tags->genre);
 
     /***********
      * Comment *
      ***********/
-		if (mp4tags->comments)
-				FileTag->comment = strdup(mp4tags->comments);
-		else
-				FileTag->comment = NULL;
+    if (mp4tags->comments)
+        FileTag->comment = g_strdup(mp4tags->comments);
 
     /**********************
      * Composer or Writer *
      **********************/
-		if (mp4tags->composer)
-				FileTag->composer = strdup(mp4tags->composer);
-		else
-				FileTag->composer = NULL;
+    if (mp4tags->composer)
+        FileTag->composer = g_strdup(mp4tags->composer);
 
     /*****************
      * Encoding Tool *
      *****************/
-		if (mp4tags->encodingTool)
-				FileTag->encoded_by = strdup(mp4tags->encodingTool);
-		else
-				FileTag->encoded_by = NULL;
+    if (mp4tags->encodedBy)
+        FileTag->encoded_by = g_strdup(mp4tags->encodedBy);
 
     /* Unimplemented
     Tempo / BPM
@@ -216,12 +213,9 @@ gboolean Mp4tag_Read_File_Tag (gchar *filename, File_Tag *FileTag)
     /***********
      * Picture *
      ***********/
-#ifdef NEWMP4
-    for (pic_num = 0; pic_num < mp4tags->artworkCount; pic_num++)
-#else
-    // There version handle only one picture!
-    if ( MP4GetMetadataCoverArt( mp4file, &coverArt, &coverSize ) )
-#endif
+    // Version 1.9.1 of mp4v2 and up handle multiple cover art
+    mp4artwork = mp4tags->artwork;
+    for (pic_num = 0; pic_num < mp4tags->artworkCount; ++pic_num, ++mp4artwork)
     {
         Picture *pic;
         
@@ -232,16 +226,16 @@ gboolean Mp4tag_Read_File_Tag (gchar *filename, File_Tag *FileTag)
             prev_pic->next = pic;
         prev_pic = pic;
 
-        pic->size = mp4tags->artwork[pic_num].size;
-        pic->data = mp4tags->artwork[pic_num].data;
-        pic->type = PICTURE_TYPE_FRONT_COVER;
+        pic->size = mp4artwork->size;
+        pic->data = g_memdup(mp4artwork->data, pic->size);
+        pic->type = PICTURE_TYPE_FRONT_COVER;/* mp4artwork->type gives image type */
         pic->description = NULL;
     }
 
 
     /* Free allocated data */
-		MP4TagsFree(mp4tags);
-    MP4Close(mp4file,0);
+    MP4TagsFree(mp4tags);
+    MP4Close(mp4file, 0);
 
     return TRUE;
 }
@@ -262,11 +256,17 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
     gchar    *filename_utf8;
     FILE     *file;
     MP4FileHandle mp4file = NULL;
-		const MP4Tags  *mp4tags;
-		MP4TagDisk disktag;
-		MP4TagTrack tracktag;
-		MP4TagArtwork artwork;
+    const MP4Tags *mp4tags = NULL;
+    MP4TagDisk mp4disk;
+    MP4TagTrack mp4track;
+    MP4TagArtwork mp4artwork; 
     gint error = 0;
+
+    /* extra initializers */
+    mp4disk.index  = 0;
+    mp4disk.total  = 0;
+    mp4track.index = 0;
+    mp4track.total = 0;
 
     if (!ETFile || !ETFile->FileTag)
         return FALSE;
@@ -284,18 +284,19 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
     fclose(file);
 
     /* Open file for writing */
-    mp4file = MP4Modify(filename,0);
+    mp4file = MP4Modify(filename, 0);
     if (mp4file == MP4_INVALID_FILE_HANDLE)
     {
         Log_Print(LOG_ERROR,_("ERROR while opening file: '%s' (%s)."),filename_utf8,_("MP4 format invalid"));
         return FALSE;
     }
 
-		mp4tags = MP4TagsAlloc();
-		if (!MP4TagsFetch(mp4tags,mp4file)) {
-			Log_Print(LOG_ERROR,_("Error reading tags from %s."),filename_utf8);
-			return FALSE;
-		}
+    mp4tags = MP4TagsAlloc();
+    if (!MP4TagsFetch(mp4tags, mp4file))
+    {
+        Log_Print(LOG_ERROR,_("ERROR reading tags from file: '%s' (%s)."),filename_utf8,_("MP4 format invalid"));
+        return FALSE;
+    }
 
     /*********
      * Title *
@@ -305,7 +306,7 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
         MP4TagsSetName(mp4tags, FileTag->title);
     }else
     {
-        MP4TagsSetName(mp4tags, NULL);
+        MP4TagsSetName(mp4tags, "");
     }
 
     /**********
@@ -316,7 +317,7 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
         MP4TagsSetArtist(mp4tags, FileTag->artist);
     }else
     {
-        MP4TagsSetArtist(mp4tags, NULL);
+        MP4TagsSetArtist(mp4tags, "");
     }
 
     /*********
@@ -327,7 +328,18 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
         MP4TagsSetAlbum(mp4tags, FileTag->album);
     }else
     {
-        MP4TagsSetAlbum(mp4tags, NULL);
+        MP4TagsSetAlbum(mp4tags, "");
+    }
+
+    /****************
+     * Album Artist *
+     ****************/
+    if (FileTag->album_artist && g_utf8_strlen(FileTag->album_artist, -1) > 0)
+    {
+        MP4TagsSetAlbumArtist(mp4tags, FileTag->album_artist);
+    }else
+    {
+        MP4TagsSetAlbumArtist(mp4tags, "");
     }
 
     /**********************
@@ -336,9 +348,6 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
     if (FileTag->disc_number && g_utf8_strlen(FileTag->disc_number, -1) > 0)
     //|| FileTag->disc_number_total && g_utf8_strlen(FileTag->disc_number_total, -1) > 0)
     {
-        uint16_t disk      = 0;
-        uint16_t disktotal = 0;
-
         /* At the present time, we manage only disk number like '1' or '1/2', we
          * don't use disk number total... so here we try to decompose */
         if (FileTag->disc_number)
@@ -349,29 +358,24 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
             {
                 // A disc_number_total was entered
                 if ( (tmp+1) && atoi(tmp+1) )
-                    disktotal = atoi(tmp+1);
+                    mp4disk.total = atoi(tmp+1);
 
                 // Fill disc_number
                 *tmp = '\0';
-                disk = atoi(dn_tmp);
+                mp4disk.index = atoi(dn_tmp);
             }else
             {
-                disk = atoi(FileTag->disc_number);
+                mp4disk.index = atoi(FileTag->disc_number);
             }
             g_free(dn_tmp);
         }
         /*if (FileTag->disc_number)
-            disk = atoi(FileTag->disc_number);
+            mp4disk.index = atoi(FileTag->disc_number);
         if (FileTag->disc_number_total)
-            disktotal = atoi(FileTag->disc_number_total);
+            mp4disk.total = atoi(FileTag->disc_number_total);
         */
-				disktag.index = disk;
-				disktag.total = disktotal;
-        MP4TagsSetDisk(mp4tags, &disktag);
-    }else
-    {
-        MP4TagsSetDisk(mp4tags, NULL);
     }
+    MP4TagsSetDisk(mp4tags, &mp4disk);
 
     /********
      * Year *
@@ -379,29 +383,23 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
     if (FileTag->year && g_utf8_strlen(FileTag->year, -1) > 0)
     {
         MP4TagsSetReleaseDate(mp4tags, FileTag->year);
-    } else {
-        MP4TagsSetReleaseDate(mp4tags, NULL);
+    }else
+    {
+        MP4TagsSetReleaseDate(mp4tags, "");
     }
 
     /*************************
      * Track and Total Track *
      *************************/
     if ( (FileTag->track       && g_utf8_strlen(FileTag->track, -1) > 0)
-	  ||   (FileTag->track_total && g_utf8_strlen(FileTag->track_total, -1) > 0 ) )
+    ||   (FileTag->track_total && g_utf8_strlen(FileTag->track_total, -1) > 0) )
     {
-        uint16_t track       = 0;
-        uint16_t track_total = 0;
         if (FileTag->track)
-            track = atoi(FileTag->track);
+            mp4track.index = atoi(FileTag->track);
         if (FileTag->track_total)
-            track_total = atoi(FileTag->track_total);
-				tracktag.index = track;
-				tracktag.total = track_total;
-        MP4TagsSetTrack(mp4tags, &tracktag);
-    } else
-    {
-        MP4TagsSetTrack(mp4tags, NULL);
+            mp4track.total = atoi(FileTag->track_total);
     }
+    MP4TagsSetTrack(mp4tags, &mp4track);
 
     /*********
      * Genre *
@@ -423,7 +421,7 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
         MP4TagsSetComments(mp4tags, FileTag->comment);
     }else
     {
-        MP4TagsSetComments(mp4tags, NULL);
+        MP4TagsSetComments(mp4tags, "");
     }
 
     /**********************
@@ -434,7 +432,7 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
         MP4TagsSetComposer(mp4tags, FileTag->composer);
     }else
     {
-        MP4TagsSetComposer(mp4tags, NULL);
+        MP4TagsSetComposer(mp4tags, "");
     }
 
     /*****************
@@ -442,10 +440,10 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
      *****************/
     if (FileTag->encoded_by && g_utf8_strlen(FileTag->encoded_by, -1) > 0)
     {
-        MP4TagsSetEncodingTool(mp4tags, FileTag->encoded_by);
+        MP4TagsSetEncodedBy(mp4tags, FileTag->encoded_by);
     }else
     {
-        MP4TagsSetEncodingTool(mp4tags, NULL);
+        MP4TagsSetEncodedBy(mp4tags, "");
     }
 
     /***********
@@ -454,31 +452,36 @@ gboolean Mp4tag_Write_File_Tag (ET_File *ETFile)
     {
         // Can handle only one picture...
         Picture *pic;
-        MP4TagsSetArtwork(mp4tags,0,NULL);
-        for( pic = FileTag->picture; pic; pic = pic->next )
+        if (mp4tags->artworkCount && mp4tags->artwork)
+            MP4TagsRemoveArtwork(mp4tags, 0);
+        MP4TagsSetArtwork(mp4tags, 0, NULL);
+        for (pic = FileTag->picture; pic; pic = pic->next)
         {
-            if( pic->type == PICTURE_TYPE_FRONT_COVER )
+            if (pic->type == PICTURE_TYPE_FRONT_COVER)
             {
-								artwork.data = pic->data;
-								artwork.size = pic->size;
-								switch (pic->type) {
-								case PICTURE_FORMAT_JPEG:
-										artwork.type = MP4_ART_JPEG;
-										break;
-								case PICTURE_FORMAT_PNG:
-										artwork.type = MP4_ART_PNG;
-										break;
-								default:
-										artwork.type = MP4_ART_UNDEFINED;
-								}
-                MP4TagsSetArtwork(mp4tags, 1, &artwork);
+                 mp4artwork.data = pic->data;
+                 mp4artwork.size = pic->size;
+                 switch (pic->type) {
+                  case PICTURE_FORMAT_JPEG:
+                     mp4artwork.type = MP4_ART_JPEG;
+                     break;
+                  case PICTURE_FORMAT_PNG:
+                     mp4artwork.type = MP4_ART_PNG;
+                     break;
+                  default:
+                     mp4artwork.type = MP4_ART_UNDEFINED;
+                 }
+                 if (mp4tags->artworkCount)
+                     MP4TagsSetArtwork(mp4tags, 0, &mp4artwork);
+                 else
+                     MP4TagsAddArtwork(mp4tags, &mp4artwork);
             }
         }
     }
 
-		MP4TagsStore(mp4tags,mp4file);
-		MP4TagsFree(mp4tags);
-    MP4Close(mp4file,0);
+    MP4TagsStore(mp4tags, mp4file);
+    MP4TagsFree(mp4tags);
+    MP4Close(mp4file, 0);
 
     if (error) return FALSE;
     else       return TRUE;
