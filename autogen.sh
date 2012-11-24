98065d10b09bb27a2dee1e79f9b0eef0cee549a0
David King <amigadave@amigadave.com>
2012-11-24 23:47:13 +0000

    Port to use intltool for internationalization

--- autogen.sh
+++ autogen.sh
@@ -4,5 +4,5 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
 mkdir -p m4
-autoreconf --force --install --verbose --warnings=all "$srcdir"
+intltoolize --copy --force --automake && autoreconf --force --install --verbose --warnings=all "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
