53292a6c8af0884553b986873496b3c950b4fbb2
David King <amigadave@amigadave.com>
2012-11-29 07:52:06 +0000

    Avoid triggering two GtkSpinButton warnings
    Setting a GtkAdjustment with a page size on a GtkSpinButton is
    deprecated. Instead, construct the GtkSpinButton with a range. This also
    avoids leaking a GtkSpinButton.

--- src/prefs.c
+++ src/prefs.c
@@ -427,7 +427,6 @@ void Open_OptionsWindow (void)
     Label = gtk_label_new (_("Max number of lines :"));
     gtk_box_pack_start(GTK_BOX(hbox),Label,FALSE,FALSE,0);
     
-    LogMaxLinesSpinButton = gtk_spin_button_new(GTK_ADJUSTMENT(gtk_adjustment_new(2.0,2.0,6.0,1.0,1.0,1.0)),1.0,0);
     LogMaxLinesSpinButton = gtk_spin_button_new_with_range(10.0,1500.0,10.0);
     gtk_box_pack_start(GTK_BOX(hbox),LogMaxLinesSpinButton,FALSE,FALSE,0);
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(LogMaxLinesSpinButton),(gfloat)LOG_MAX_LINES);
@@ -601,7 +600,7 @@ void Open_OptionsWindow (void)
         "the number '0' as padding to obtain a number with 'n' digits (Ex. with two digits : '05', "
         "'09', '10' …). Else it keeps the 'raw' track value."));
 
-    NumberTrackFormatedSpinButton = gtk_spin_button_new(GTK_ADJUSTMENT(gtk_adjustment_new(2.0,2.0,6.0,1.0,1.0,1.0)),1.0,0);
+    NumberTrackFormatedSpinButton = gtk_spin_button_new_with_range(2.0,6.0,1.0);
     gtk_box_pack_start(GTK_BOX(hbox),NumberTrackFormatedSpinButton,FALSE,FALSE,0);
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(NumberTrackFormatedSpinButton),(gfloat)NUMBER_TRACK_FORMATED_SPIN_BUTTON);
     g_signal_connect(G_OBJECT(NumberTrackFormated),"toggled",G_CALLBACK(Number_Track_Formated_Toggled),NULL);
