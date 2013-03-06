26ddaa7c4be188b18bd5ffbdc8b4691f2c204df5
Dominique Leuenberger <dimstar@opensuse.org>
2013-03-06 21:11:50 +0100

    Update FSF Address, bug 695321

--- src/id3lib/id3_bugfix.h
+++ src/id3lib/id3_bugfix.h
@@ -15,7 +15,7 @@
 
  * You should have received a copy of the GNU Library General Public License
  * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
  * The id3lib authors encourage improvements and optimisations to be sent to
  * the id3lib coordinator.  Please see the README file for details on where to
