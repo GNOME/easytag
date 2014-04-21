52531351eb2e33a750a25f0625f12991874e9896
David King <amigadave@amigadave.com>
2014-04-21 16:18:12 +0100

    Fix compilation if Ogg support is not available
    Add some ifdefs to check whether ENABLE_OGG is defined. Remove leftover
    prototype in WavPack tag code.

--- src/ogg_tag.h
+++ src/ogg_tag.h
@@ -24,6 +24,11 @@
 
 
 #include <glib.h>
+
+#include "config.h"
+
+#ifdef ENABLE_OGG
+
 #include "vcedit.h"
 #include "et_core.h"
 
@@ -33,9 +38,10 @@
 gboolean ogg_tag_read_file_tag (gchar *filename, File_Tag *FileTag,
                                 GError **error);
 gboolean ogg_tag_write_file_tag (ET_File *ETFile, GError **error);
-void
-et_add_file_tags_from_vorbis_comments (vorbis_comment *vc, File_Tag *FileTag,
-                                       const gchar *filename_utf8);
-void
-et_add_vorbis_comments_from_file_tags (vorbis_comment *vc, File_Tag *FileTag);
+
+void et_add_file_tags_from_vorbis_comments (vorbis_comment *vc, File_Tag *FileTag, const gchar *filename_utf8);
+void et_add_vorbis_comments_from_file_tags (vorbis_comment *vc, File_Tag *FileTag);
+
+#endif /* ENABLE_OGG */
+
 #endif /* __OGG_TAG_H__ */
