9dbe534dd0599ede2bd4a2a27d0add8fb404ac2b
David King <amigadave@amigadave.com>
2014-06-11 20:01:55 +0100

    Initialize gettext in Nautilus extension startup

--- nautilus-extension/nautilus-easytag.c
+++ nautilus-extension/nautilus-easytag.c
@@ -1,18 +1,19 @@
-/*
- *  Copyright (C) 2014 Victor A. Santos <victoraur.santos@gmail.com>
+/* EasyTAG - tag editor for audio files
+ * Copyright (C) 2014 Victor A. Santos <victoraur.santos@gmail.com>
+ * Copyright (C) 2014 David King <amigadave@amigadave.com>
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "config.h"
@@ -282,6 +283,8 @@ nautilus_easytag_init (NautilusEasytag *self)
 static void
 nautilus_easytag_class_init (NautilusEasytagClass *class)
 {
+    bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 }
 
 static void
