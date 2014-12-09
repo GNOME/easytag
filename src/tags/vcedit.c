21773c4bde88e344a5cbde8a3c4d76b4d7fe3aeb
David King <amigadave@amigadave.com>
2014-12-09 23:01:02 +0000

    Relicense libapetag and vcedit.c to GPLv2+
    Invoke Section 3 of the LGPL, and apply the terms of the GPL to all the
    LGPL portions of code inside EasyTAG, which include the APE tag reading
    code (libapetag) and the Vorbis comment parsing code (vcedit.c).

--- src/tags/vcedit.c
+++ src/tags/vcedit.c
@@ -1,12 +1,20 @@
-/* This program is licensed under the GNU Library General Public License, version 2,
- * a copy of which is included with this program (LICENCE.LGPL).
+/* EasyTAG - tag editor for audio files
+ * Copyright (C) 2014 David King <amigadave@amigadave.com>
+ * Copyright (C) 2000-2001 Michael Smith <msmith@xiph.org>
  *
- * (c) 2000-2001 Michael Smith <msmith@xiph.org>
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
  *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  *
- * Comment editing backend, suitable for use by nice frontend interfaces.
- *
- * last modified: $Id: vcedit.c,v 1.23 2003/09/03 07:58:05 calc Exp $
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #include "config.h" /* For definition of ENABLE_OGG. */
