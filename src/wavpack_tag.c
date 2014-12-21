a7250d53e8d4e782043e14de6f35cb54982326da
David King <amigadave@amigadave.com>
2014-12-21 22:33:25 +0000

    Fix string leak when reading WavPack tags

--- src/wavpack_tag.c
+++ src/wavpack_tag.c
@@ -138,11 +138,20 @@ gboolean Wavpack_Tag_Read_File_Tag (gchar *filename, File_Tag *FileTag)
 
     if (field2 && FileTag->disc_total == NULL)
     {
-        FileTag->disc_total = et_disc_number_to_string (atoi (Try_To_Validate_Utf8_String (field2)));
+        gchar *tmp;
+
+        tmp = Try_To_Validate_Utf8_String (field2);
+        FileTag->disc_total = et_disc_number_to_string (atoi (tmp));
+        g_free (tmp);
     }
+
     if (length > 0 && FileTag->disc_number == NULL)
     {
-        FileTag->disc_number = et_disc_number_to_string (atoi (Try_To_Validate_Utf8_String (field)));
+        gchar *tmp;
+
+        tmp = Try_To_Validate_Utf8_String (field);
+        FileTag->disc_number = et_disc_number_to_string (atoi (tmp));
+        g_free (tmp);
     }
 
     memset (field, '\0', MAXLEN);
@@ -172,11 +181,20 @@ gboolean Wavpack_Tag_Read_File_Tag (gchar *filename, File_Tag *FileTag)
 
     if (field2 && FileTag->track_total == NULL)
     {
-        FileTag->track_total = et_track_number_to_string (atoi (Try_To_Validate_Utf8_String (field2)));
+        gchar *tmp;
+
+        tmp = Try_To_Validate_Utf8_String (field2);
+        FileTag->track_total = et_track_number_to_string (atoi (tmp));
+        g_free (tmp);
     }
+
     if (length > 0 && FileTag->track == NULL)
     {
-        FileTag->track = et_track_number_to_string (atoi (Try_To_Validate_Utf8_String (field)));
+        gchar *tmp;
+
+        tmp = Try_To_Validate_Utf8_String (field);
+        FileTag->track = et_track_number_to_string (atoi (tmp));
+        g_free (tmp);
     }
 
     memset (field, '\0', MAXLEN);
