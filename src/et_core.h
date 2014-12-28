103c20223cdbf9e6870d371fd3a7a82a317dca96
David King <amigadave@amigadave.com>
2014-12-28 22:32:37 +0000

    Split majority of et_core.c out into new file.c

--- src/et_core.h
+++ src/et_core.h
@@ -20,17 +20,13 @@
 #ifndef ET_CORE_H_
 #define ET_CORE_H_
 
-#include "setting.h"
+#include <glib.h>
 
 G_BEGIN_DECLS
 
-#include <glib.h>
 #include <gdk/gdk.h>
 
-#include "core_types.h"
-#include "file_description.h"
-#include "file_tag.h"
-#include "picture.h"
+#include "file.h"
 
 /*
  * Colors Used (see declaration into et_core.c)
@@ -38,63 +34,9 @@ G_BEGIN_DECLS
 extern GdkRGBA RED;
 
 /*
- * Description of each item of the FileNameList list
- */
-typedef struct _File_Name File_Name;
-struct _File_Name
-{
-    guint key;
-    gboolean saved;        /* Set to TRUE if this filename had been saved */
-    gchar *value;          /* The filename containing the full path and the extension of the file */
-    gchar *value_utf8;     /* Same than "value", but converted to UTF-8 to avoid multiple call to the convertion function */
-    gchar *value_ck;       /* Collate key of "value_utf8" to speed up comparaison */
-};
-
-/*
- * Description of each item of the ETFileList list
- */
-typedef struct _ET_File ET_File;
-struct _ET_File
-{
-    guint IndexKey;           /* Value used to display the position in the list (and in the BrowserList) - Must be renumered after resorting the list - This value varies when resorting list */
-
-    guint ETFileKey;          /* Primary key to identify each item of the list (no longer used?) */
-
-    time_t FileModificationTime;            /* Save modification time of the file */
-
-    const ET_File_Description *ETFileDescription;
-    gchar               *ETFileExtension;   /* Real extension of the file (keeping the case) (should be placed in ETFileDescription?) */
-    ET_File_Info        *ETFileInfo;        /* Header infos: bitrate, duration, ... */
-
-    GList *FileNameCur;       /* Points to item of FileNameList that represents the current value of filename state (i.e. file on hard disk) */
-    GList *FileNameNew;       /* Points to item of FileNameList that represents the new value of filename state */
-    GList *FileNameList; /* Contains the history of changes about the filename. */
-    GList *FileNameListBak;   /* Contains items of FileNameList removed by 'undo' procedure but have data currently saved (for example, when you save your last changes, make some 'undo', then make new changes) */
-    
-    GList *FileTag;           /* Points to the current item used of FileTagList */
-    GList *FileTagList;       /* Contains the history of changes about file tag data */
-    GList *FileTagListBak;    /* Contains items of FileTagList removed by 'undo' procedure but have data currently saved */
-    
-};
-
-
-
-/*
- * Description of each item of the ETHistoryFileList list
- */
-typedef struct _ET_History_File ET_History_File;
-struct _ET_History_File
-{
-    ET_File *ETFile;           /* Pointer to item of ETFileList changed */
-};
-
-
-
-/*
  * Description of all variables, lists needed by EasyTAG
  */
-typedef struct _ET_Core ET_Core;
-struct _ET_Core
+typedef struct
 {
     // The main list of files
     GList *ETFileList;                  // List of ALL FILES (ET_File) loaded in the directory and sub-directories (List of ET_File) (This list musn't be altered, and points always to the first item)
@@ -102,134 +44,27 @@ struct _ET_Core
     // The list of files organized by artist then album
     GList *ETArtistAlbumFileList;
 
-
     // Displayed list (part of the main list of files displayed in BrowserList) (used when displaying by Artist & Album) 
     GList *ETFileDisplayedList;                 // List of files displayed (List of ET_File from ETFileList / ATArtistAlbumFileList) | !! May not point to the first item!!
     GList *ETFileDisplayedListPtr;              // Pointer to the current item of ETFileDisplayedList used with ET_Displayed_File_List_First, ET_Displayed_File_List_Previous
     guint  ETFileDisplayedList_Length;          // Contains the length of the displayed list
     gfloat ETFileDisplayedList_TotalSize;       // Total of the size of files in displayed list (in bytes)
     gulong ETFileDisplayedList_TotalDuration;   // Total of duration of files in displayed list (in seconds)
-    
+
     // Displayed item
     ET_File *ETFileDisplayed;           // Pointer to the current ETFile displayed in EasyTAG (may be NULL)
 
 
     // History list
     GList *ETHistoryFileList;           // History list of files changes for undo/redo actions
-};
+} ET_Core;
 
 extern ET_Core *ETCore; /* Main pointer to structure needed by EasyTAG. */
-
-
-
-/**************
- * Prototypes *
- **************/
 
 void ET_Core_Create     (void);
 void ET_Core_Initialize (void);
 void ET_Core_Free       (void);
 void ET_Core_Destroy    (void);
-
-gboolean ET_File_Is_Supported (const gchar *filename);
-GList   *ET_Add_File_To_File_List        (gchar *filename);
-gboolean ET_Remove_File_From_File_List   (ET_File *ETFile);
-
-gboolean ET_Create_Artist_Album_File_List      (void);
-gboolean ET_Remove_File_From_File_List         (ET_File *ETFile);
-
-gboolean ET_Check_If_File_Is_Saved (const ET_File *ETFile);
-gboolean ET_Check_If_All_Files_Are_Saved (void);
-
-ET_File      *ET_File_Item_New       (void);
-File_Name    *ET_File_Name_Item_New  (void);
-void      ET_Free_File_List_Item (ET_File *ETFile);
-
-gboolean ET_Set_Field_File_Name_Item    (gchar **FileNameField, gchar *value);
-gboolean ET_Set_Filename_File_Name_Item (File_Name *FileName, const gchar *filename_utf8, const gchar *filename);
-
-/* FIXME: Move to file_tag.h. */
-gboolean ET_Copy_File_Tag_Item (const ET_File *ETFile, File_Tag *FileTag);
-void ET_Copy_File_Tag_Item_Other_Field (const ET_File *ETFile, File_Tag *FileTag);
-
-GList   *ET_Displayed_File_List_First       (void);
-GList   *ET_Displayed_File_List_Previous    (void);
-GList   *ET_Displayed_File_List_Next        (void);
-GList   *ET_Displayed_File_List_Last        (void);
-GList *ET_Displayed_File_List_By_Etfile (const ET_File *ETFile);
-
-gboolean ET_Set_Displayed_File_List         (GList *ETFileList);
-
-void     ET_Display_File_Data_To_UI (ET_File *ETFile);
-void     ET_Save_File_Data_From_UI  (ET_File *ETFile);
-gboolean ET_Save_File_Tag_To_HD (ET_File *ETFile, GError **error);
-
-guint ET_Undo_Key_New (void);
-
-gboolean ET_Undo_File_Data          (ET_File *ETFile);
-gboolean ET_Redo_File_Data          (ET_File *ETFile);
-gboolean ET_File_Data_Has_Undo_Data (const ET_File *ETFile);
-gboolean ET_File_Data_Has_Redo_Data (const ET_File *ETFile);
-
-ET_File *ET_Undo_History_File_Data          (void);
-ET_File *ET_Redo_History_File_Data          (void);
-gboolean ET_History_File_List_Has_Undo_Data (void);
-gboolean ET_History_File_List_Has_Redo_Data (void);
-
-gboolean ET_Manage_Changes_Of_File_Data          (ET_File *ETFile, File_Name *FileName, File_Tag *FileTag);
-void     ET_Mark_File_Name_As_Saved              (ET_File *ETFile);
-void ET_Update_Directory_Name_Into_File_List (const gchar *last_path, const gchar *new_path);
-gboolean ET_File_Name_Convert_Character          (gchar *filename_utf8);
-gchar *ET_File_Name_Generate (const ET_File *ETFile, const gchar *new_file_name);
-guint ET_Get_Number_Of_Files_In_Directory (const gchar *path_utf8);
-
-GList *ET_Sort_File_List (GList *ETFileList, EtSortMode Sorting_Type);
-void ET_Sort_Displayed_File_List_And_Update_UI (EtSortMode Sorting_Type);
-gint ET_Comp_Func_Sort_File_By_Ascending_Filename (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Filename (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Creation_Date (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Creation_Date (const ET_File *ETFile1, const ET_File *ETFile2);
-gint et_comp_func_sort_file_by_ascending_disc_number (const ET_File *ETFile1,
-                                                      const ET_File *ETFile2);
-gint et_comp_func_sort_file_by_descending_disc_number (const ET_File *ETFile1,
-                                                       const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Track_Number (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Track_Number (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Title (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Title (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Album_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Album_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Album (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Album (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Year (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Year (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Genre (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Genre (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Comment (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Comment (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Composer (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Composer (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Orig_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Orig_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Copyright (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Copyright (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Url (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Url (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_Encoded_By (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_Encoded_By (const ET_File *ETFile1, const ET_File *ETFile2);
-
-gint ET_Comp_Func_Sort_File_By_Ascending_File_Type (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_File_Type (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_File_Size (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_File_Size (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_File_Duration (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_File_Duration (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_File_Bitrate (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_File_Bitrate (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Ascending_File_Samplerate (const ET_File *ETFile1, const ET_File *ETFile2);
-gint ET_Comp_Func_Sort_File_By_Descending_File_Samplerate (const ET_File *ETFile1, const ET_File *ETFile2);
 
 G_END_DECLS
 
---
+++ src/file.h
@@ -0,0 +1,181 @@
+/* EasyTAG - tag editor for audio files
+ * Copyright (C) 2014  David King <amigadave@amigadave.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef ET_FILE_H_
+#define ET_FILE_H_
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+#include "core_types.h"
+#include "file_description.h"
+#include "file_tag.h"
+#include "setting.h"
+
+/*
+ * Description of each item of the FileNameList list
+ */
+typedef struct
+{
+    guint key;
+    gboolean saved;        /* Set to TRUE if this filename had been saved */
+    gchar *value;          /* The filename containing the full path and the extension of the file */
+    gchar *value_utf8;     /* Same than "value", but converted to UTF-8 to avoid multiple call to the convertion function */
+    gchar *value_ck;       /* Collate key of "value_utf8" to speed up comparaison */
+} File_Name;
+
+/*
+ * Description of each item of the ETFileList list
+ */
+typedef struct
+{
+    guint IndexKey;           /* Value used to display the position in the list (and in the BrowserList) - Must be renumered after resorting the list - This value varies when resorting list */
+
+    guint ETFileKey;          /* Primary key to identify each item of the list (no longer used?) */
+
+    time_t FileModificationTime;            /* Save modification time of the file */
+
+    const ET_File_Description *ETFileDescription;
+    gchar               *ETFileExtension;   /* Real extension of the file (keeping the case) (should be placed in ETFileDescription?) */
+    ET_File_Info        *ETFileInfo;        /* Header infos: bitrate, duration, ... */
+
+    GList *FileNameCur;       /* Points to item of FileNameList that represents the current value of filename state (i.e. file on hard disk) */
+    GList *FileNameNew;       /* Points to item of FileNameList that represents the new value of filename state */
+    GList *FileNameList; /* Contains the history of changes about the filename. */
+    GList *FileNameListBak;   /* Contains items of FileNameList removed by 'undo' procedure but have data currently saved (for example, when you save your last changes, make some 'undo', then make new changes) */
+
+    GList *FileTag;           /* Points to the current item used of FileTagList */
+    GList *FileTagList;       /* Contains the history of changes about file tag data */
+    GList *FileTagListBak;    /* Contains items of FileTagList removed by 'undo' procedure but have data currently saved */
+} ET_File;
+
+/*
+ * Description of each item of the ETHistoryFileList list
+ */
+typedef struct
+{
+    ET_File *ETFile;           /* Pointer to item of ETFileList changed */
+} ET_History_File;
+
+GList   *ET_Add_File_To_File_List        (gchar *filename);
+gboolean ET_Remove_File_From_File_List   (ET_File *ETFile);
+gboolean ET_Free_File_List (void);
+
+gboolean ET_Create_Artist_Album_File_List      (void);
+gboolean ET_Free_Artist_Album_File_List (void);
+gboolean ET_Remove_File_From_File_List         (ET_File *ETFile);
+
+gboolean ET_Check_If_File_Is_Saved (const ET_File *ETFile);
+gboolean ET_Check_If_All_Files_Are_Saved (void);
+
+ET_File      *ET_File_Item_New       (void);
+File_Name    *ET_File_Name_Item_New  (void);
+void      ET_Free_File_List_Item (ET_File *ETFile);
+
+gboolean ET_Set_Field_File_Name_Item    (gchar **FileNameField, gchar *value);
+gboolean ET_Set_Filename_File_Name_Item (File_Name *FileName, const gchar *filename_utf8, const gchar *filename);
+
+/* FIXME: Move to file_tag.h. */
+gboolean ET_Copy_File_Tag_Item (const ET_File *ETFile, File_Tag *FileTag);
+void ET_Copy_File_Tag_Item_Other_Field (const ET_File *ETFile, File_Tag *FileTag);
+
+GList   *ET_Displayed_File_List_First       (void);
+GList   *ET_Displayed_File_List_Previous    (void);
+GList   *ET_Displayed_File_List_Next        (void);
+GList   *ET_Displayed_File_List_Last        (void);
+GList *ET_Displayed_File_List_By_Etfile (const ET_File *ETFile);
+
+gboolean ET_Set_Displayed_File_List         (GList *ETFileList);
+gboolean ET_Free_Displayed_File_List (void);
+
+void     ET_Display_File_Data_To_UI (ET_File *ETFile);
+void     ET_Save_File_Data_From_UI  (ET_File *ETFile);
+gboolean ET_Save_File_Tag_To_HD (ET_File *ETFile, GError **error);
+
+guint ET_Undo_Key_New (void);
+
+gboolean ET_Undo_File_Data          (ET_File *ETFile);
+gboolean ET_Redo_File_Data          (ET_File *ETFile);
+gboolean ET_File_Data_Has_Undo_Data (const ET_File *ETFile);
+gboolean ET_File_Data_Has_Redo_Data (const ET_File *ETFile);
+
+ET_File *ET_Undo_History_File_Data          (void);
+ET_File *ET_Redo_History_File_Data          (void);
+gboolean ET_History_File_List_Has_Undo_Data (void);
+gboolean ET_History_File_List_Has_Redo_Data (void);
+gboolean ET_Free_History_File_List (void);
+
+gboolean ET_Manage_Changes_Of_File_Data          (ET_File *ETFile, File_Name *FileName, File_Tag *FileTag);
+void     ET_Mark_File_Name_As_Saved              (ET_File *ETFile);
+void ET_Update_Directory_Name_Into_File_List (const gchar *last_path, const gchar *new_path);
+gboolean ET_File_Name_Convert_Character          (gchar *filename_utf8);
+gchar *ET_File_Name_Generate (const ET_File *ETFile, const gchar *new_file_name);
+guint ET_Get_Number_Of_Files_In_Directory (const gchar *path_utf8);
+
+GList *ET_Sort_File_List (GList *ETFileList, EtSortMode Sorting_Type);
+void ET_Sort_Displayed_File_List_And_Update_UI (EtSortMode Sorting_Type);
+gint ET_Comp_Func_Sort_File_By_Ascending_Filename (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Filename (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Creation_Date (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Creation_Date (const ET_File *ETFile1, const ET_File *ETFile2);
+gint et_comp_func_sort_file_by_ascending_disc_number (const ET_File *ETFile1,
+                                                      const ET_File *ETFile2);
+gint et_comp_func_sort_file_by_descending_disc_number (const ET_File *ETFile1,
+                                                       const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Track_Number (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Track_Number (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Title (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Title (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Album_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Album_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Album (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Album (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Year (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Year (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Genre (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Genre (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Comment (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Comment (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Composer (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Composer (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Orig_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Orig_Artist (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Copyright (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Copyright (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Url (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Url (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_Encoded_By (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_Encoded_By (const ET_File *ETFile1, const ET_File *ETFile2);
+
+gint ET_Comp_Func_Sort_File_By_Ascending_File_Type (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_File_Type (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_File_Size (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_File_Size (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_File_Duration (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_File_Duration (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_File_Bitrate (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_File_Bitrate (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Ascending_File_Samplerate (const ET_File *ETFile1, const ET_File *ETFile2);
+gint ET_Comp_Func_Sort_File_By_Descending_File_Samplerate (const ET_File *ETFile1, const ET_File *ETFile2);
+
+G_END_DECLS
+
+#endif /* !ET_FILE_H_ */
