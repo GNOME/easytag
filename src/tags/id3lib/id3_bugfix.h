6224dff545be6ae7776d13f4ed37b148edd9c1a9
David King <amigadave@amigadave.com>
2015-01-04 14:26:40 +0000

    Relicense id3lib wrapper to GPLv2+
    Invoke Section 3 of the LGPL, and apply the terms of the GPL to all the
    LGPL portions of id3lib code inside EasyTAG.

--- src/tags/id3lib/id3_bugfix.h
+++ src/tags/id3lib/id3_bugfix.h
@@ -1,42 +1,32 @@
-/* 
- * id3lib: a software library for creating and manipulating id3v1/v2 tags
- * Copyright 1999, 2000  Scott Thomas Haug
- * Copyright 2002 Thijmen Klok (thijmen@id3lib.org)
+/* EasyTAG - tag editor for audio files
+ * Copyright (C) 2014 David King <amigadave@amigadave.com>
+ * Copyright (C) 1999, 2000  Scott Thomas Haug
+ * Copyright (C) 2002 Thijmen Klok (thijmen@id3lib.org)
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Library General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
  *
- * This library is distributed in the hope that it will be useful, but WITHOUT
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
- * License for more details.
-
- * You should have received a copy of the GNU Library General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
- * The id3lib authors encourage improvements and optimisations to be sent to
- * the id3lib coordinator.  Please see the README file for details on where to
- * send such submissions.  See the AUTHORS file for a list of people who have
- * contributed to id3lib.  See the ChangeLog file for a list of changes to
- * id3lib.  These files are distributed with id3lib at
- * http://download.sourceforge.net/id3lib/
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#ifndef _ID3LIB_BUGFIX_H_
-#define _ID3LIB_BUGFIX_H_
-
+#ifndef ET_ID3LIB_BUGFIX_H_
+#define ET_ID3LIB_BUGFIX_H_
 
 #ifdef ENABLE_ID3LIB
 
-#include "id3.h"
+#include <glib.h>
+#include <id3.h>
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif /* __cplusplus */
+G_BEGIN_DECLS
 
   ID3_C_EXPORT bool                  CCONV ID3Field_SetEncoding    (ID3Field *field, ID3_TextEnc enc);
   ID3_C_EXPORT ID3_TextEnc           CCONV ID3Field_GetEncoding    (const ID3Field *field);
@@ -46,11 +36,8 @@ extern "C"
 
   ID3_C_EXPORT const Mp3_Headerinfo* CCONV ID3Tag_GetMp3HeaderInfo (ID3Tag *tag);
   
-#ifdef __cplusplus
-}
-#endif /*__cplusplus*/
-
+G_END_DECLS
 
 #endif /* ENABLE_ID3LIB */
 
-#endif /* _ID3LIB_BUGFIX_H_ */
+#endif /* ET_ID3LIB_BUGFIX_H_ */
