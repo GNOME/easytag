40b0223062200881b9cefc61b54a1d9b74c15b10
David King <amigadave@amigadave.com>
2015-01-04 14:26:40 +0000

    Relicense id3lib wrapper to GPLv2+
    Invoke Section 3 of the LGPL, and apply the terms of the GPL to all the
    LGPL portions of id3lib code inside EasyTAG.

--- src/id3lib/c_wrapper.cpp
+++ src/id3lib/c_wrapper.cpp
@@ -1,29 +1,23 @@
-// id3lib: a C++ library for creating and manipulating id3v1/v2 tags
-// Copyright 1999, 2000  Scott Thomas Haug
-// Copyright 2002 Thijmen Klok (thijmen@id3lib.org)
+/* EasyTAG - tag editor for audio files
+ * Copyright (C) 2014 David King <amigadave@amigadave.com>
+ * Copyright (C) 1999, 2000  Scott Thomas Haug
+ * Copyright (C) 2002 Thijmen Klok (thijmen@id3lib.org)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
-// This library is free software; you can redistribute it and/or modify it
-// under the terms of the GNU Library General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or (at your
-// option) any later version.
-//
-// This library is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
-// License for more details.
-//
-// You should have received a copy of the GNU Library General Public License
-// along with this library; if not, write to the Free Software Foundation,
-// Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-// The id3lib authors encourage improvements and optimisations to be sent to
-// the id3lib coordinator.  Please see the README file for details on where to
-// send such submissions.  See the AUTHORS file for a list of people who have
-// contributed to id3lib.  See the ChangeLog file for a list of changes to
-// id3lib.  These files are distributed with id3lib at
-// http://download.sourceforge.net/id3lib/
-
-//#include <string.h>
 #include "config.h"
 
 #ifdef ENABLE_ID3LIB
