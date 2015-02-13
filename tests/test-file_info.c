fc4526c271bc0a3634b235850f10f17f520a6651
David King <amigadave@amigadave.com>
2015-02-13 23:01:41 +0000

    Add a trivial ET_File_Info test

---
+++ tests/test-file_info.c
@@ -0,0 +1,41 @@
+/* EasyTAG - tag editor for audio files
+ * Copyright (C) 2015 David King <amigadave@amigadave.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "file_info.h"
+
+static void
+file_info_new (void)
+{
+    ET_File_Info *file_info;
+
+    file_info = et_file_info_new ();
+
+    g_assert (file_info);
+
+    et_file_info_free (file_info);
+}
+
+int
+main (int argc, char** argv)
+{
+    g_test_init (&argc, &argv, NULL);
+
+    g_test_add_func ("/file_info/new", file_info_new);
+
+    return g_test_run ();
+}
