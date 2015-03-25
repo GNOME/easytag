e57617990f468a68a9c2bc8ee6579c53db91f141
David King <amigadave@amigadave.com>
2015-03-25 16:26:36 +0100

    Improve log area minimum size
    Do not allow the paned to shrink the log area, and set a minimum height
    on the scrolled window.

--- src/application_window.c
+++ src/application_window.c
@@ -1727,7 +1727,7 @@ et_application_window_init (EtApplicationWindow *self)
 
     /* Log */
     priv->log_area = et_log_area_new ();
-    gtk_paned_pack2 (GTK_PANED (priv->vpaned), priv->log_area, FALSE, TRUE);
+    gtk_paned_pack2 (GTK_PANED (priv->vpaned), priv->log_area, FALSE, FALSE);
 
     /* Horizontal box for Status bar + Progress bar */
     hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
