23ce98865177a48bc1578af878a718c07e4e2449
David King <amigadave@amigadave.com>
2015-06-30 17:52:26 +0100

    Add for loop condition to File_Tag check
    Avoid a crash when checking the difference between two files without
    images.

--- src/file_tag.c
+++ src/file_tag.c
@@ -453,7 +453,8 @@ et_file_tag_detect_difference (const File_Tag *FileTag1,
     }
 
     /* Picture */
-    for (pic1 = FileTag1->picture, pic2 = FileTag2->picture; ;
+    for (pic1 = FileTag1->picture, pic2 = FileTag2->picture;
+         pic1 || pic2;
          pic1 = pic1->next, pic2 = pic2->next)
     {
         if (et_picture_detect_difference (pic1, pic2))
